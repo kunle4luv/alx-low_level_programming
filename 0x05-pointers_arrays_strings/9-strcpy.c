b69078c3b50c02786437d90a7cf90a96f25d5d06
Ayokunle Ajisafe <ajisafeayokunle@gmail.com>
2023-07-25 04:05:57 -0700

    9-stropy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,12 +10,17 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int l = 0;
+	int x = 0;
 
-	while (src[i] != '\0')
+	while (*(src + l) != '\0')
 	{
-		dest[i] = src[i];
-		i++;
+		l++;
 	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
 	return (dest);
 }
