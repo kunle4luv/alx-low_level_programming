40845ac8fe17753a4e5e0ff59b2537307d3acc05
Ayokunle Ajisafe <ajisafeayokunle@gmail.com>
2023-07-25 04:01:25 -0700

    8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -6,20 +6,17 @@
   * @n: number of elements of the araay to be printed
   * Return: Nothing
   */
-
 void print_array(int *a, int n)
 {
 	int i;
 
-	i = 0;
-	while (i < n)
+	for (i = 0; i < (n - 1); i++)
 	{
-		if (i == n - 1)
-		{
-			printf("%d\n", a[i]);
-			break;
-		}
 		printf("%d, ", a[i]);
-		i++;
 	}
+		if (i == (n - 1))
+		{
+			printf("%d", a[n - 1]);
+		}
+			printf("\n");
 }
