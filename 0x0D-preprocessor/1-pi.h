931e2bb9b2763c43c056db1fd4bf6d0889a2f869
Ayokunle Ajisafe <ajisafeayokunle@gmail.com>
2023-08-15 20:03:40 -0700

    C - Preprocessor

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * file: 1-pi.h
+ * Desc: A file that defines a macro named pi
+ * 	as an abbreviation for the token 3.14159265359.
+ *
+ */
+
+#define PI 3.14159265359
+
+#endif
