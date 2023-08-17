823a192510080d50b2673bc6efe5508eb90cf12c
Ayokunle Ajisafe <ajisafeayokunle@gmail.com>
2023-08-16 21:29:03 -0700

    C-function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,7 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
- * primt_name - A function that prints a name
+ * print_name - A function that prints a name
  * @name:Name to be printed out
  * @f: pointer to a function that prints a name
  */
