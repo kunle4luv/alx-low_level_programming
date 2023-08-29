31e2af7081779a7fe5e659d7b6c773cfa5ae986d
Ayokunle Ajisafe <ajisafeayokunle@gmail.com>
2023-08-29 04:38:40 -0700

    C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+#include <string.h>
+
+/**
+ * print_listint - prints all the elements of a listint_t list.
+ * @h:the head of linklist node
+ *
+ * Return: the number of nodes
+ *
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * add_nodeint - A dunction that add node
+  * at beginning of a listint_t list.
+  * @head: head of double pointer
+  * @n: int add list.
+  *
+  * Return: NULL if it failed
+  */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *nnd;
+
+	if (head == NULL)
+		return (NULL);
+	nnd = malloc(sizeof(listint_t));
+	if (nnd == NULL)
+		return (NULL);
+	nnd->n = n;
+	nnd->next = *head;
+	*head = nnd;
+	return (nnd);
+}
