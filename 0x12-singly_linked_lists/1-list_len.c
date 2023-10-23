09e44ddea59b5950894101de80f76a40b1bb1942
Ayokunle Ajisafe <ajisafeayokunle@gmail.com>
2023-10-23 16:58:12 -0700

    List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * list_len - A function that finds the number of elements
+ * in a linked list_t list.
+ * @h: The linked list_t list
+ * Return: number of element in h.
+ */
+size_t list_len(constlist_t *h)
+{
+	size_t element = 0;
+
+	while (h)
+	{
+		element++;
+		h = h->next;
+	}
+	return (element);
+}
