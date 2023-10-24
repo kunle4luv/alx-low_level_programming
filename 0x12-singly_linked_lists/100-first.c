f2efd2b531058ff79fc38768e39a9cc86412512f
Ayokunle Ajisafe <ajisafeayokunle@gmail.com>
2023-10-23 17:27:35 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * hare - Prints a string before the
+ * main function is executed.
+ * Return : nothing
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
