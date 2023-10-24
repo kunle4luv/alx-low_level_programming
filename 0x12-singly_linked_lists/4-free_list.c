32c25952a081b0bf41c538c526a5a9fa7f45c16d
Ayokunle Ajisafe <ajisafeayokunle@gmail.com>
2023-10-23 17:21:37 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - A function that Frees a list_t list.
+ * @head: A pointer to the list_t list.
+ *
+ * Return: Nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+}
