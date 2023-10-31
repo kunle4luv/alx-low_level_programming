c5d38dd372cce6daa8f3381edbbe919f85536f46
Ayokunle Ajisafe <ajisafeayokunle@gmail.com>
2023-10-30 18:21:53 -0700

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * read_textfile - A function that read atext file 
+ * and prints it to stdout
+ * @filename: The name of the file to read
+ * @letters: The number of letter to read and print
+ *
+ * Return: The Number of letter read and printed,
+ * and o on failure.
+ */
+ssize_t read_textfile(const char *filename, size_t letter)
+{
+	int o, bytesRead, bytesWrite;
+	char *buffer;
+
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	bytesRead = read(o, buffer, letters);
+	bytesWrite = write(STDOUT_FILENO, buffer, bytesRead);
+
+	if (o == -1 || bytesRead == -1 || bytesWrite == -1 || bytesRead != bytesWrite)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+	return (bytesWrite);
+}
